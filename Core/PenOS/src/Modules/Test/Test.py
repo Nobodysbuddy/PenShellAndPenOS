5bb74bd3ff29821bc40312b2015650d1a60d36cc
NobodysBuddy <aryan.aparna.thakur@gmail.com>
2020-06-20 18:13:18 +0530

    Today's Work Is Finally Over!

---
+++ Core/PenOS/src/Modules/Test/Test.py
@@ -0,0 +1,1 @@
+print('Doggo Bites The Man With A Stick!')
--- Core/PenOS/src/__init__.py
+++ Core/PenOS/src/__init__.py
@@ -1,3 +1,31 @@
 print("\n\nPenOS!\n")
 print("Welcome To PenOS,")
 print("a open-source Command Line Interface For Launching And Developing Applications")
+print("------------------------------------------------------------------------------")
+
+modules = []
+
+
+if modules == []:
+    print('You Dont Have Any Modules Installed. Please Use The Install Command To Install One.')
+
+else:
+    print('You Have Modules! You Are Good To Go!')
+    
+print('\n If You Dont Have Any Modules Install One To Do Things With PenOS.')
+print("Starting PenOS...")
+print("------------------------------------------------------------------------\n\n")
+    
+while True:
+    command = input("====>")
+    if command == "install":
+        pname = input("Package's Name: ")
+        try:
+            filename = 'Modules/' + pname + '/' + pname + '.py'
+            exec(open(filename).read())
+        except:
+            print("Cant Find Filename Or Module Doesn't Exist")
+        
+        
+    elif command == "quit":
+        break
